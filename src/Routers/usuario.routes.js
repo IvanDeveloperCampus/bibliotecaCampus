723ba6e907fe68042060e7ef7574dc9ffcf69343
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-07-24 08:42:34 -0500

    feat: :sparkles: Implementacion de un "login" para simular acceso y autenticacion
    Se implemento con el fin de simular un inicio de sesion y generar un token unico para solo permitir acceder a los recursos a usuarios autenticados

--- src/Routers/usuario.routes.js
+++ src/Routers/usuario.routes.js
@@ -1,9 +1,36 @@
 import { Router } from "express";
 import getConnection from "../db/database.js";
+import { generateAccessToken, verifyToken } from "../Middlwares/jwt.js";
 
 const usuarioRouter = Router();
 
-usuarioRouter.get("/getUsuarios", async (req, res) => {
+usuarioRouter.post("/login", async (req, res) => {
+  try {
+    const { email } = req.body;
+    const con = await getConnection();
+
+    const [rows] = await con.execute("SELECT * FROM usuario WHERE email=?", [
+      email,
+    ]);
+    if (rows.length >= 1) {
+      const user = rows[0];
+      const accessToken = generateAccessToken(user);
+      res.cookie("token", accessToken, { httpOnly: true });
+      res.status(200).send("Ha ingresado Correctamente.");
+    } else {
+      res.status(400).send("Email Incorrecto, revisa la informacion");
+    }
+  } catch (error) {
+    res.status(500).send("Error en el servidor." + error.message);
+  }
+});
+
+usuarioRouter.get('/lagout', (req, res)=>{
+  res.clearCookie('token');
+  res.send('Sesion Cerrada');
+});
+
+usuarioRouter.get("/getUsuarios", verifyToken, async (req, res) => {
   try {
     const con = await getConnection();
     const [result] = await con.execute("SELECT nombre, email FROM usuario");
@@ -12,7 +39,7 @@ usuarioRouter.get("/getUsuarios", async (req, res) => {
     }
     res.status(200).send(result);
   } catch (error) {
-    res.status(500).send(error.message)
+    res.status(500).send(error.message);
   }
 });
 
