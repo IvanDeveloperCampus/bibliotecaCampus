ddb32aaacb6d54b89790dfc5718f54e513054d65
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-07-24 08:01:17 -0500

    feat: :sparkles: Catorceavo EndPoint
    Listar los préstamos realizados por un usuario (ejemplo: Juan Pérez).

--- src/Routers/prestamo.routes.js
+++ src/Routers/prestamo.routes.js
@@ -17,4 +17,20 @@ prestamoRouter.get("/getPrestamos", async (req, res) => {
   }
 });
 
+prestamoRouter.get("/getPrestamosUsuarioEspecifico", async (req, res) => {
+    try {
+        const { nombreApellido }=req.query
+      const con = await getConnection();
+      const [result] = await con.execute("SELECT * from prestamo INNER JOIN usuario ON prestamo.id_usuario=usuario.id_usuario WHERE concat(nombre,' ',apellido)=?", [nombreApellido]);
+      if (result.length === 0) {
+        return res.status(204).send(`No hay prestamos del usuario ${nombreApellido}`);
+      }
+      res.status(200).send(result);
+    } catch (error) {
+      res.status(500).send(error)
+    }
+  });
+
+
+
 export default prestamoRouter;
