b23096d10e651a796df067687ca123aa8e5a56b1
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-07-24 07:00:44 -0500

    feat: :sparkles: Configuracion conexion a la base de datos

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from "express";
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app=express();
+app.use(express.json())
 
 app.listen(5001, ()=>{
     console.log("server corriendo");
---
+++ src/db/database.js
@@ -0,0 +1,23 @@
+import mysql2  from 'mysql2/promise';
+
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const pool=mysql2.createPool({
+    host: process.env.HOSTNAME,
+    user: process.env.NAME_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+})
+
+const getConnection=async()=>{
+    try {
+        const connection=await pool.getConnection();
+        return connection;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export default getConnection;
