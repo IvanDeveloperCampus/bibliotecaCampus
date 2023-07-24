44ff88056eda852c52cf86917f20df7209a340af
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-07-24 09:25:48 -0500

    feat: :sparkles: Diecisieteavo EndPoint
    Listar los autores de nacionalidad española.

--- src/Routers/autores.routes.js
+++ src/Routers/autores.routes.js
@@ -15,4 +15,15 @@ autorRouter.get("/getAutores", verifyToken,async (req, res) => {
   } catch (error) {}
 });
 
+autorRouter.get("/getAutoresEspanioles", verifyToken,async (req, res) => {
+  try {
+    const con = await getConnection();
+    const [result] = await con.execute("SELECT * FROM autor WHERE nacionalidad='Española'");
+    if (result.length === 0) {
+      return res.status(204).send(`No hay autores de nacionalidad española`);
+    }
+    res.status(200).send(result);
+  } catch (error) {}
+});
+
 export default autorRouter;
